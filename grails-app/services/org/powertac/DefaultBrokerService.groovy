5c93fdf6d23d9d0bb7bf938ff6ce3612ff480d4f
ddauer <ddauer@gmail.com>
2011-04-07 16:22:19 +0200

    Add default broker server sekelton

---
+++ grails-app/services/org/powertac/DefaultBrokerService.groovy
@@ -0,0 +1,12 @@
+package org.powertac
+
+import org.powertac.common.Broker
+
+class DefaultBrokerService extends Broker {
+
+  static transactional = true
+
+  def serviceMethod() {
+
+  }
+}
---
+++ test/unit/org/powertac/DefaultBrokerServiceTests.groovy
@@ -0,0 +1,13 @@
+package org.powertac
+
+import grails.test.*
+
+class DefaultBrokerServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+}
