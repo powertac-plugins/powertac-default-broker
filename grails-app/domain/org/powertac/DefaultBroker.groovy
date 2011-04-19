ca95e04129d948c8dca8328a87551b3e6ab51c88
Chris Flath <flath@fzi.de>
2011-04-19 11:22:13 +0200

    added tariffMarketService.processTariff functionality

--- grails-app/domain/org/powertac/DefaultBroker.groovy
+++ grails-app/domain/org/powertac/DefaultBroker.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2009-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package org.powertac
 
 import org.powertac.common.Broker
@@ -6,11 +22,18 @@ import org.powertac.common.TariffSpecification
 import org.powertac.common.enumerations.PowerType
 import org.powertac.common.msg.SimStart
 import org.powertac.common.Competition
-
+import org.powertac.common.interfaces.TariffMarket
+/**
+ * The default broker represents the legacy power provider. It provides
+ * basic tariffs for consumption and production as specified in the
+ * competition configuration
+ * @author Christoph Flath, KIT
+ */
 class DefaultBroker extends Broker {
 
   double consumptionRate
   double productionRate
+  TariffMarket tariffMarketService
 
   DefaultBroker() {
     this.local = true
@@ -26,13 +49,14 @@ class DefaultBroker extends Broker {
     (broker: this, powerType: PowerType.CONSUMPTION)
     Rate defaultConsumptionRate = new Rate(value: consumptionRate)
     defaultConsumptionTariffSpecification.addToRates(defaultConsumptionRate)
+    tariffMarketService.processTariff(defaultConsumptionTariffSpecification)
 
     /* Default Production Tariff */
     TariffSpecification defaultProductionTariffSpecification = new TariffSpecification
     (broker: this, powerType: PowerType.PRODUCTION)
     Rate defaultProductionRate = new Rate(value: productionRate)
     defaultProductionTariffSpecification.addToRates(defaultProductionRate)
-
+    tariffMarketService.processTariff(defaultProductionTariffSpecification)
   }
 
   @Override
@@ -41,11 +65,11 @@ class DefaultBroker extends Broker {
     if (object instanceof SimStart) {
       publishDefaultTariffs()
     }
-    // set default tariffs according to competition configuration
+    // set default tariffs according to competition configuration parameter map entries
     if (object instanceof Competition)
     {
-    consumptionRate = object.defaultConsumptionRate
-    productionRate = object.defaultProductionRate
+    consumptionRate = object.parameterMap.defaultConsumptionRate
+    productionRate = object.parameterMap.defaultProductionRate
     }
     }
   }
